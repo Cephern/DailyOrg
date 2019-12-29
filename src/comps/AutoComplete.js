915d2d806e62aa7ec801177b0f677b0a71b7d2b1
JagMax <chronophobia95@gmail.com>
2019-12-30 00:09:36 +0300

    Ignore case on input. Set focus to next input after entering

--- src/comps/AutoComplete.js
+++ src/comps/AutoComplete.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
-const AutoComplete = ({ name, setName, suggestions, focus, setFocus }) => {
+const AutoComplete = ({ name, setName, suggestions, focus }) => {
 
     let suggestionsList;
     if (focus) {
         suggestionsList = name ? (
-            [...suggestions.filter((suggestion) => suggestion.name.toLowerCase().includes(name)).map((suggestion) => (
+            [...suggestions.filter((suggestion) => suggestion.name.toLowerCase().includes(name.toLowerCase())).map((suggestion) => (
                 <li onClick={(e) => {
                     setName(suggestion.name);
+                    document.querySelector('#cost').focus();
                 }} key={suggestion.id}>{suggestion.name}</li>)
             )]
         ) : (
